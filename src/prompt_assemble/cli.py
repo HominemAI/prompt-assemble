b2e33f4f1a664ad1a112b09da68b54f794c5ad30
AgentSanchez <adam.sanchez@adamsanchez.net>
2026-03-06 17:41:42 -0800

    PyPi Version Bump

--- src/prompt_assemble/cli.py
+++ src/prompt_assemble/cli.py
@@ -75,7 +75,7 @@ def main() -> None:
     parser.add_argument(
         '--version',
         action='version',
-        version='%(prog)s 0.1.0',
+        version='%(prog)s 0.2.0',
     )
 
     args = parser.parse_args()
